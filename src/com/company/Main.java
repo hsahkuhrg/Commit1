6dd81ac4c1a68fc10917f14495faf2cc4dae3363
Vitalik Vlasenko <vitaliy.vlasenko@cts.kh.ua>
2018-02-28 09:43:41 +0200

    Commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("args = [" + args + "]");
-                // jfgg
+        String d = "dbeg";
+        System.out.println("args = [" + args + "]");
     }
 }
